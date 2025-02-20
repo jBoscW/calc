7783dadb1a947c48491f4265559514131e279301
jBoscW <jbd393@nyu.edu>
2025-02-20 16:46:03 -0300

    Get core working, but there are bugs

--- script.js
+++ script.js
@@ -46,41 +46,56 @@ function newDigit(event) {
     };
 };
 
-function newOperator(event) {
+function newOperatorPressed(event) {
     const opPressed = event.target.dataset.op
 
-    if (num1 && num2){
-        switch (opPressed) {
-            case 'add':
-                console.log(1);
-                break;
-            case 'subtract':
-                console.log(2);
-                break;
-            case 'multiply':
-                console.log(3);
-                break;
-            case 'divide':
-                console.log(4);
-                break;
-            case 'equal':
-                console.log(5);
-                break
+    if (num1 && num2) {
+        operate(num1, num2, operation);
+        display.textContent = result;
+        reset(opPressed)
         }
-    }
     else {
-        operation = opPressed;
+        if (opPressed != 'equals')
+            operation = opPressed;
     }
 }
 
 function operate(a, b, oper) {
-    // add here
-    return result;
+    if (parseInt(a) && parseInt(b)) {
+        switch (oper) {
+            case 'add': 
+                result = add(a,b); 
+                break;
+            case 'subtract': 
+                result = subtract(a,b);
+                break;
+            case 'multiply': 
+                result = multiply(a,b);
+                break;
+            case 'divide': 
+                result = divide(a,b);
+                break;
+            case 'equal': 
+                result = operate(a, b, operation);
+                break;
+        };
+    } else {
+        result = NaN;
+    }
 };
 
- 
-
+// reset num1, num2, result, and operation and reassign 
+function reset(opPressed) {
+    if (opPressed === 'equal') {
+        
+    } else {
+        num1 = result;
+        operation = opPressed;
+        num2 = null;
+        result = null;
+    };
+}
 
 //Event Listeners
 numbers.forEach(numButton => numButton.addEventListener('click', newDigit));
-operators.forEach(opButton => opButton.addEventListener('click', newOperator));
+operators.forEach(opButton => opButton.addEventListener('click', newOperatorPressed));
